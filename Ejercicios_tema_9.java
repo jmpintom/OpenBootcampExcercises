4deb992a071e94b24cc8680b40363ccf515688ea
Jehider Miguel Pinto Montes <105828851+jmpintom@users.noreply.github.com>
2023-01-20 14:26:47 -0500

    Update Ejercicios_tema_9.java

--- Ejercicios_tema_9.java
+++ Ejercicios_tema_9.java
@@ -12,24 +12,24 @@
 
 // Una vez hecho esto, haz lo mismo con la clase Trabajador que herede de Persona, y con una variable salario que solo tenga la clase Trabajador.
 
-public class Main {
+public class Ejercicio9 {
     public static void main(String[] args){
 
         Cliente jaimito = new Cliente();
         jaimito.setNombre("Jaimito Gutierrez");
         jaimito.setTelefono(312564254);
         jaimito.setEdad(25);
-        jaimito.isCredito(True);
-        System.out.println("Cliente");
-        System.out.println("Nombre:"+jaimito.getNombre()+" Tel:"+jaimito.getTelefono()+" edad:"+jaimito.getEdad()+" Credito:"+jaimito.getCredito());
-      
-      
+        jaimito.setCredito(true);
+        System.out.println("Cliente:");
+        System.out.println("Nombre:"+jaimito.getNombre()+" Tel:"+jaimito.getTelefono()+" edad:"+jaimito.getEdad()+" Credito:"+jaimito.isCredito());
+
+        System.out.println("");
         Trabajador pedro = new Trabajador();
         pedro.setNombre("Pedro Sanchez");
         pedro.setTelefono(310554654);
         pedro.setEdad(30);
         pedro.setSalario(1800);
-        System.out.println("Trabajador");
+        System.out.println("Trabajador:");
         System.out.println("Nombre:"+pedro.getNombre()+" Tel:"+pedro.getTelefono()+" edad:"+pedro.getEdad()+" Credito:"+pedro.getSalario());
 
     }
@@ -37,56 +37,55 @@ public class Main {
 
 
 class Persona {
-  
-  private int edad;
-  private int telefono;
-  private String nombre;
-  
-  public int getEdad() {
+
+    private int edad;
+    private int telefono;
+    private String nombre;
+
+    public int getEdad() {
         return edad;
     }
-  public void setEdad(int edad) {
-      this.edad = edad;
-  }
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
 
 
-  public String getNombre() {
-      return this.nombre;
-  }
-  public void setNombre(String nombre) {
-      this.nombre = nombre;
-  }
+    public String getNombre() {
+        return this.nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 
 
-  public int getTelefono() {
-      return telefono;
-  }
-  public void setTelefono(int telefono) {
-      this.telefono = telefono;
-  } 
+    public int getTelefono() {
+        return telefono;
+    }
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
 }
 
-class Cliente extends Persona {
-  
-  private booleam credito;
-   
-  public booleam isCredito() {
-      return this.credito;
-  }
-  public void setCredito(String credito) {
-      this.credito = credito;
-  }
+class Cliente extends Persona2 {
+
+    private boolean credito;
+
+    public boolean isCredito() {
+        return this.credito;
+    }
+    public void setCredito(boolean credito) {
+        this.credito = credito;
+    }
 }
 
 
-class Trabajador extends Persona {
-  
-  private int salario;
-   
-  public int getSalario() {
-      return this.salario;
-  }
-  public void setSalario(int salario) {
-      this.salario = salario;
-  }
-}
+class Trabajador extends Persona2 {
+
+    private int salario;
+
+    public int getSalario() {
+        return this.salario;
+    }
+    public void setSalario(int salario) {
+        this.salario = salario;
+    }
